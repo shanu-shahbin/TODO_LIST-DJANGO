5bc82008b74704dd9b6a01230fc62903800e131d
shanu shahbin <shanushahbin11@gmail.com>
2023-10-18 09:45:56 +0530

    front end

--- todo/front_end/urls.py
+++ todo/front_end/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from front_end import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+
+    path('', views.home, name="home"),
+    path('delete/<int:taskid>/', views.delete, name="delete"),
+    path('update/<int:id>/', views.update, name="update")
 
 ]
--- todo/front_end/views.py
+++ todo/front_end/views.py
@@ -1,7 +1,31 @@
+from django.shortcuts import render, redirect
 
-from django.shortcuts import render
+from .forms import TodoForm
+from .models import Todo
 
 
-# Create your views here.
-def index(request):
-    pass
+def home(request):
+    task_all = Todo.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('task', '')
+        date = request.POST.get('date', '')
+        task = Todo(title=name, date=date)
+        task.save()
+    return render(request, 'home.html', {'task_all': task_all})
+
+
+def delete(request, taskid):
+    task = Todo.objects.get(id=taskid)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    return render(request, 'delete.html')
+
+
+def update(request, id):
+    task = Todo.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render (request,'edit.html', {'form':form, 'task':task})
